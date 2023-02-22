afc03dffbace7ca6e87c69c97c618f7374f1bc7b
ScaraMouche <114559016+Leviachann@users.noreply.github.com>
2023-02-22 08:10:13 +0400

    ev tapsirigi 1

---
+++ part2ofhw1/part2ofhw1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace part2ofhw1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dontclick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dontclick
+            // 
+            this.dontclick.Location = new System.Drawing.Point(316, 112);
+            this.dontclick.Name = "dontclick";
+            this.dontclick.Size = new System.Drawing.Size(75, 23);
+            this.dontclick.TabIndex = 0;
+            this.dontclick.Text = "Do not Click";
+            this.dontclick.UseVisualStyleBackColor = true;
+            this.dontclick.Click += new System.EventHandler(this.dontclick_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dontclick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button dontclick;
+    }
+}
+
---
+++ part2ofhw1/part2ofhw1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace part2ofhw1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dontclick.Location = new Point(400, 400);
+        }
+
+        private void dontclick_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            dontclick.Location = new Point(r.Next(0, 500), r.Next(0, 400));
+        }
+    }
+}
